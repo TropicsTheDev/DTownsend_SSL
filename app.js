0a03dc5eeb6d238173fb80fef722a1c0f080dff3
Douglas Townsend <dougymt@gmail.com>
2020-07-22 23:09:05 -0400

    got the lecture code working

--- app.js
+++ app.js
@@ -3,42 +3,60 @@ const http = require("http");
 const path = require("path");
 const url = require("url");
 
-http
-  .createServer((req, res) => {
-    const parsed = url.parse(req.url);
-    const filename = path.parse(parsed.pathname);
-    const filen = filename.name == "" ? "index" : filename.name; //actual name of the file
-    const ext = filename.ext=="" ? ".html" : filename.ext;
-    const dir = filename.dir=="/" ? "" : filename.dir + "/";
-    const page = filename.base=="" ? "site/index.html" : filename.base;
-    
-    const file = (dir + filen + ext).replace("/", "");
-    console.log(filename);
-    // console.log(filen, "filen");
-    // console.log(ext, "ext");
-    // console.log(dir, "dir");
-    // console.log(page, "page");
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
 
-    const mimeTypes = {
-        ".html": "text.html",
-        ".js": "text/javascript",
-        ".css": "text/css",
-        ".png": "image/png",
-        ".jpg": "image.jpeg",
-        ".gif": "image.gif"
-    }
+const ejs = require("ejs");
+const router = express.Router();
+const app = express();
 
-    fs.readFile(file, (error, data) => {
-      if(page){
-           console.log("is page");
-          if(mimeTypes.hasOwnProperty(ext)){
-              res.writeHead(200, {"Content-Type": mimeTypes[ext] });
-              res.write("<script>let page='" + page + "';</script>");
-              res.end(data, "utf-8");
-          }
-      }
-    });
-  })
-  .listen("8080", ()=> {
-      console.log("Server is running at port 8080");
-  });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.engine("ejs", require("ejs").__express);
+
+router.get("/", (req, res) => {
+  res.render("index", { pagename: "Home" });
+});
+router.get("/about", (req, res) => {
+  res.render("about", { pagename: "About" });
+});
+router.get("/contact", (req, res) => {
+  res.render("contact", { pagename: "Contact" });
+});
+router.get("/post", (req, res) => {
+  res.render("post", { pagename: "Post" });
+});
+
+router.post("/login", (req, res) => {
+  //console.log(req.body);
+  const errors = [];
+
+  //validations
+  if (req.body.email == "") {
+    errors.push("Email is required");
+    console.log("failed");
+  }
+  if (req.body.password == "") {
+    errors.push("password is required");
+  }
+  if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(req.body.email)) {
+    errors.push("Email is not valid");
+  } //is an email address
+  if (
+    !/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/.test(
+      req.body.password
+    )
+  ) {
+    errors.push("Password is not valid");
+  } //fits password conditions
+
+  console.log(errors.length)
+
+  res.render("index", { pagename: "Home", errors: errors });
+});
+
+app.use(express.static("public"));
+app.use("/", router);
+const server = app.listen("8080");
